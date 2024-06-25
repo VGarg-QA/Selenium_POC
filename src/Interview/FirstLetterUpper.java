b4d259eb01a13cc5bc371c9875174d368f0fa186
VGarg-QA <vishal.garg094@gmail.com>
2024-06-25 11:44:38 +0530

    practice commmit

--- src/Interview/FirstLetterUpper.java
+++ src/Interview/FirstLetterUpper.java
@@ -1,2 +1,30 @@
-package Interview;public class FirstLetterUpper {
+package Interview;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Scanner;
+
+public class FirstLetterUpper {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a String : ");
+        String str = sc.nextLine();
+        System.out.print("You have entered : " + str + "\n");
+
+       // Check if the input string is not empty
+        if (!str.isEmpty()) {
+            // Get the first character and convert it to uppercase
+            String firstLetterUpper = str.substring(0, 1).toUpperCase() + str.substring(1);
+
+            System.out.println("String with first letter capitalized: " + firstLetterUpper);
+        } else {
+            System.out.println("Input string is empty.");
+        }
+
+        sc.close();
+    }
 }
+
+
